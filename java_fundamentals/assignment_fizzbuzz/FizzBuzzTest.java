6920636d32c733253f32bae2ece99dff54d5bbfd
Rob Moore <moore.re@gmail.com>
2021-05-07 15:07:37 -0500

    Completed FizzBuzz Assignment

---
+++ java_fundamentals/assignment_fizzbuzz/FizzBuzzTest.java
@@ -0,0 +1,11 @@
+package assignment_fizzbuzz;
+
+public class FizzBuzzTest {
+    public static void main(String[] args) {
+        FizzBuzz example = new FizzBuzz();
+        System.out.println(example.fizzBuzz(3));
+        System.out.println(example.fizzBuzz(5));
+        System.out.println(example.fizzBuzz(15));
+        System.out.println(example.fizzBuzz(2));
+    }
+}
